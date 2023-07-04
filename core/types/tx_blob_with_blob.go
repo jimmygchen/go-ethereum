1f79afa9575e68752a1f4c7e478135948cd9a9d9
Marius van der Wijden <m.vanderwijden@live.de>
2023-07-04 09:49:15 +0200

    core, eth: working on encoding

--- core/types/tx_blob_with_blob.go
+++ core/types/tx_blob_with_blob.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"bytes"
 	"io"
 
 	"github.com/ethereum/go-ethereum/crypto/kzg4844"
@@ -33,36 +34,94 @@ type innerType struct {
 	Proofs      []kzg4844.Proof
 }
 
+// DecodeRLP implements rlp.Decoder
+func (tx *BlobTxWithBlobs) DecodeRLP2(s *rlp.Stream) error {
+	kind, size, err := s.Kind()
+	switch {
+	case err != nil:
+		return err
+	case kind == rlp.List:
+		// It's a legacy transaction.
+		var inner LegacyTx
+		err := s.Decode(&inner)
+		if err == nil {
+			tx.Transaction.setDecoded(&inner, rlp.ListSize(size))
+		}
+		return err
+	default:
+		// It's an EIP-2718 typed TX envelope.
+		var b []byte
+		if b, err = s.Bytes(); err != nil {
+			return err
+		}
+		if b[0] == BlobTxType {
+			var blobTypedTx innerType
+			//err := s.Decode(&blobTypedTx)
+			err := rlp.DecodeBytes(b[1:], &blobTypedTx)
+			if err == nil {
+				tx.Transaction = *NewTx(blobTypedTx.BlobTx)
+				tx.Blobs = blobTypedTx.Blobs
+				tx.Commitments = blobTypedTx.Commitments
+				tx.Proofs = blobTypedTx.Proofs
+			}
+			return err
+		}
+		inner, err := tx.Transaction.decodeTyped(b)
+		if err == nil {
+			tx.Transaction.setDecoded(inner, uint64(len(b)))
+		}
+		return err
+	}
+}
+
 func (tx *BlobTxWithBlobs) DecodeRLP(s *rlp.Stream) error {
+	return tx.DecodeRLP2(s)
 	var typedTx Transaction
 	err := s.Decode(&typedTx)
 	if err == nil {
 		tx.Transaction = typedTx
 		return nil
 	}
-
 	var blobTypedTx innerType
-	if s.Decode(&blobTypedTx) == nil {
+	if err := s.Decode(&blobTypedTx); err == nil {
 		tx.Transaction = *NewTx(blobTypedTx.BlobTx)
 		tx.Blobs = blobTypedTx.Blobs
 		tx.Commitments = blobTypedTx.Commitments
 		tx.Proofs = blobTypedTx.Proofs
 		return nil
 	}
-
 	return err
 }
 
+type innerType2 struct {
+	BlobTx      *BlobTx
+	Blobs       []kzg4844.Blob
+	Commitments []kzg4844.Commitment
+	Proofs      []kzg4844.Proof
+}
+
+type wrapper struct {
+	TxType byte
+	Inner  innerType2
+}
+
 func (tx *BlobTxWithBlobs) EncodeRLP(w io.Writer) error {
+	var b bytes.Buffer
+	if err := tx.Transaction.EncodeRLP(&b); err != nil {
+		return err
+	}
 	if tx.Transaction.Type() != BlobTxType {
-		return tx.Transaction.EncodeRLP(w)
+		_, err := w.Write(b.Bytes())
+		return err
 	}
-	inner := innerType{
-		BlobTx:      tx.inner.(*BlobTx),
-		Blobs:       tx.Blobs,
-		Commitments: tx.Commitments,
-		Proofs:      tx.Proofs,
-	}
-	w.Write([]byte{tx.Type()})
-	return rlp.Encode(w, inner)
+	return rlp.Encode(w, &wrapper{
+		TxType: BlobTxType,
+		Inner: innerType2{
+			BlobTx:      tx.Transaction.inner.(*BlobTx),
+			Blobs:       tx.Blobs,
+			Commitments: tx.Commitments,
+			Proofs:      tx.Proofs,
+		}})
+	//_, err := w.Write(b.Bytes())
+	//return err
 }
